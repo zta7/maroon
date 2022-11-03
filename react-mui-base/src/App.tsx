24ee9bf12aab6ac041d091d02c16399c76baa8a6
zta <zta538218@gmail.com>
2022-11-04 01:20:57 +0800

    w

---
+++ react-mui-base/src/App.tsx
@@ -0,0 +1,11 @@
+import ButtonUnstyled from '@mui/base/ButtonUnstyled';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonUnstyled>123</ButtonUnstyled>
+    </div>
+  )
+}
+
+export default App
